b76ccbcd3a56f478b6aa745751861a8ef7d57e70
Orion Reed <orionreedt@gmail.com>
2020-02-16 12:40:20 +0000

    Added .gitignore

--- directory.rb
+++ directory.rb
@@ -1,14 +1,49 @@
+@students = []
+
+def interactive_menu
+  loop do
+    show_options
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def show_options
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
+end
+
 def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
-  students = []
   name = gets.chomp
   until name.empty?
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
+  @students << { name: name, cohort: :november }
+  puts "Now we have #{@students.count} students"
+  name = gets.chomp
+end
 end
 
 def print_header
@@ -16,17 +51,23 @@ def print_header
   puts '-------------'
 end
 
-def print(names)
-  names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_students
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+  puts ""
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    file.puts [student[:name], student[:cohort]].join(",")
+  end
+  file.close
+end
+
+interactive_menu
