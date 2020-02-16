f39419878e0718d4e99c5545e415ea167dc2170f
Orion Reed <orionreedt@gmail.com>
2020-02-16 11:28:48 +0000

    Refactored to take user input

--- directory.rb
+++ directory.rb
@@ -1,19 +1,32 @@
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  students = []
+  name = gets.chomp
+  until name.empty?
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
 end
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts 'The students of Villains Academy'
+  puts '-------------'
+end
+
+def print(names)
+  names.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
